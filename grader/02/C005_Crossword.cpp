7fa1baec52e579b98008a5e6bb09c6f4d014a130
PatatoBit <pathonscript@gmail.com>
2025-09-29 21:53:51 +0700

    crossword solved

---
+++ grader/02/C005_Crossword.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1, s2;
+	int matchingFirstAt, matchingSecondAt;
+	cin >> s1 >> s2;
+
+	// Find matching letter
+	for (int i = 0; i < s1.length(); i++)
+	{
+		for (int j = 0; j < s2.length(); j++)
+		{
+			if (s1[i] == s2[j])
+			{
+				matchingFirstAt = i;
+				matchingSecondAt = j;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < s2.length(); i++)
+	{
+		if (i != matchingSecondAt)
+		{
+			// Print out empty space
+			for (int j = 0; j < matchingFirstAt; j++)
+			{
+				cout << " ";
+			}
+			cout << s2[i] << endl;
+		}
+		else
+		{
+			cout << s1 << endl;
+		}
+	}
+
+	return 0;
+}
